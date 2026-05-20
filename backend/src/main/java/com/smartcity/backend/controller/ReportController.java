08e82a70688fd06608ed27c424692fe6a27e4abc
omar-kurde <omar@example.com>
2026-05-20 09:53:46 +0300

    Ihate

--- backend/src/main/java/com/smartcity/backend/controller/ReportController.java
+++ backend/src/main/java/com/smartcity/backend/controller/ReportController.java
@@ -56,15 +56,14 @@ public class ReportController {
 
     @GetMapping("all/summary")
     public ResponseEntity<List<ReportSummary>> getAllReportSummary(double northLat, double northLng, double southLat, double southLng, int zoom) {
-        ResponseEntity<List<ReportSummary>> x= ResponseEntity.ok(reportService.getAllReportsSummaryInViewPort(northLat, northLng, southLat, southLng, zoom));
-        System.out.println(x);
-        return x;
+        return ResponseEntity.ok(reportService.getAllReportsSummaryInViewPort(northLat, northLng, southLat, southLng, zoom));
     }
 
 
     @GetMapping("all/viewport")
     public ResponseEntity<List<ReportResponse>> getAllReports(double northLat, double northLng, double southLat, double southLng, int zoom) {
-        return null;
+        List<ReportResponse> temp =  reportService.getAllReportsInViewPort(northLat, northLng, southLat, southLng, zoom);
+        return ResponseEntity.ok(temp);
     }
     // -------------------------------------------------------------------------
     // GET /api/report/all
